6799bf72ac9562cd52859df905ba3f379acef0e7
Swolebrain <thecodingteacher@gmail.com>
2016-06-07 19:16:13 -0400

    finished temp loading and added icons

---
+++ script.js
@@ -0,0 +1,21 @@
+$.ajax({
+    url: "http://ipinfo.io/json",
+    success: function(resp, txt, xhr){
+      var loc = resp.city+", "+resp.region+", "+resp.country;
+      $("#location").html(loc);
+      console.log(resp);
+      
+      var weatherurl = `http://api.openweathermap.org/data/2.5/weather?zip=${resp.postal},us&APPID=a8f589720a5a69bbdd147c645d28f48b`;
+      $.ajax({
+        url: weatherurl,
+        success: function(resp2, txt2, xhr2){
+          console.log(resp2);
+          var celsius = resp2.main.temp-273;
+          celsius = Math.round(celsius);
+          $("#temp").html(celsius);
+        }
+      });
+    }
+});
+
+
